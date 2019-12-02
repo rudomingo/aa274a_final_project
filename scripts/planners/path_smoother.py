aa99be967b1cdf5b9263d73db239b79959e4df4f
Ron Domingo <rondomingo@DN0a22c6f0.SUNet>
2019-12-02 10:11:15 -0800

    Added path smoother

--- scripts/planners/path_smoother.py
+++ scripts/planners/path_smoother.py
@@ -19,7 +19,35 @@ def compute_smoothed_traj(path, V_des, alpha, dt):
     Hint: Use splrep and splev from scipy.interpolate
     """
     ########## Code starts here ##########
-    raise NotImplementedError # REPLACE THIS FUNCTION WITH YOUR IMPLEMENTATION
+    path = np.array(path)
+    x_values = path[:,0]
+    y_values = path[:,1]
+    times = []
+    times.append(0)
+    for i in range(path.shape[0]-1):
+        distance = np.linalg.norm(path[i+1] - path[i])
+        times.append((distance/V_des) + times[-1])
+
+    t_smoothed = np.arange(0, times[-1], dt)
+
+    tck_x = scipy.interpolate.splrep(times, x_values, s=alpha)
+    tck_y = scipy.interpolate.splrep(times, y_values, s=alpha)
+    x = scipy.interpolate.splev(t_smoothed, tck_x)
+    x_dot = scipy.interpolate.splev(t_smoothed, tck_x, der=1)
+    x_ddot = scipy.interpolate.splev(t_smoothed, tck_x, der=2)
+    y = scipy.interpolate.splev(t_smoothed, tck_y)
+    y_dot = scipy.interpolate.splev(t_smoothed, tck_y, der=1)
+    y_ddot = scipy.interpolate.splev(t_smoothed, tck_y, der=2)
+    theta = np.arctan2(y_dot, x_dot)
+
+    traj_smoothed = np.zeros((t_smoothed.shape[0],7))
+    traj_smoothed[:,0] = x
+    traj_smoothed[:,1] = y
+    traj_smoothed[:,2] = theta
+    traj_smoothed[:,3] = x_dot
+    traj_smoothed[:,4] = y_dot
+    traj_smoothed[:,5] = x_ddot
+    traj_smoothed[:,6] = y_ddot
     ########## Code ends here ##########
 
     return traj_smoothed, t_smoothed
