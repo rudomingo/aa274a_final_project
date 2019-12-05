9c2459e15c58cfdd5c6cb914a41fa2bb636763c9
ambyld <bylard@stanford.edu>
2019-12-05 09:51:25 -0800

    Changed the text size in the eta

--- scripts/navigator.py
+++ scripts/navigator.py
@@ -411,11 +411,11 @@ class Navigator:
 
         marker.scale.x = 0.4
         marker.scale.y = 0.1
-        marker.scale.z = 0.1
+        marker.scale.z = 0.4
 
         marker.color.a = 1.0 # Don't forget to set the alpha!
-        marker.color.r = 1.0
-        marker.color.g = 1.0
+        marker.color.r = 0.02
+        marker.color.g = 0.84
         marker.color.b = 1.0
         marker.text = "ETA to destination: {}".format(self.current_plan_duration)
         self.vis_pub.publish(marker)
