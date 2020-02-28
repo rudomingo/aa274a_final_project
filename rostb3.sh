4e3e8685d991a180677d4645591d6b18d7117ed3
JeffersonAero <holly.dinkel@gmail.com>
2020-02-28 09:38:53 -0800

    New detector viz and rostb3 files

--- rostb3.sh
+++ rostb3.sh
@@ -1,3 +1,3 @@
 export ROS_MASTER_URI=http://missgalaxy.local:11311
-export ROS_IP=$(ifconfig wlp5s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-export ROS_HOSTNAME=$(ifconfig wlp5s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+export ROS_IP=$(ifconfig wlp5s0 | grep 'inet ' | awk '{ print $2}')
+export ROS_HOSTNAME=$(ifconfig wlp5s0 | grep 'inet ' | awk '{ print $2}')
